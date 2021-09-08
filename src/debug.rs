803b80a9f59faa4d6dfaa58f1062eaf2a04afbf3
matiasop <movalle4@uc.cl>
2021-09-07 23:57:46 -0300

    add logic tiles refactor

--- src/debug.rs
+++ src/debug.rs
@@ -1,58 +1,28 @@
 use crate::logic::Board;
-use crate::structs::{PieceType, Square};
+use crate::structs::PieceType;
 
 pub fn print_board(board: &Board) {
-    for (i, square) in board.squares.iter().enumerate() {
-        let character = match square {
-            Square {
-                piece: Some(PieceType::Pawn),
-                color: Some(false),
-            } => "P",
-            Square {
-                piece: Some(PieceType::Knight),
-                color: Some(false),
-            } => "N",
-            Square {
-                piece: Some(PieceType::Bishop),
-                color: Some(false),
-            } => "B",
-            Square {
-                piece: Some(PieceType::Rook),
-                color: Some(false),
-            } => "R",
-            Square {
-                piece: Some(PieceType::Queen),
-                color: Some(false),
-            } => "Q",
-            Square {
-                piece: Some(PieceType::King),
-                color: Some(false),
-            } => "K",
-            Square {
-                piece: Some(PieceType::Pawn),
-                color: Some(true),
-            } => "p",
-            Square {
-                piece: Some(PieceType::Knight),
-                color: Some(true),
-            } => "n",
-            Square {
-                piece: Some(PieceType::Bishop),
-                color: Some(true),
-            } => "b",
-            Square {
-                piece: Some(PieceType::Rook),
-                color: Some(true),
-            } => "r",
-            Square {
-                piece: Some(PieceType::Queen),
-                color: Some(true),
-            } => "q",
-            Square {
-                piece: Some(PieceType::King),
-                color: Some(true),
-            } => "k",
-            _ => "-",
+    for (i, tile) in board.tiles.iter().enumerate() {
+        let character = match tile.piece {
+            None => "-",
+            Some(piece) => match piece.color {
+                true => match piece.piece_type {
+                    PieceType::Pawn => "P",
+                    PieceType::Knight => "N",
+                    PieceType::Bishop => "B",
+                    PieceType::Rook => "R",
+                    PieceType::Queen => "Q",
+                    PieceType::King => "K",
+                },
+                false => match piece.piece_type {
+                    PieceType::Pawn => "p",
+                    PieceType::Knight => "n",
+                    PieceType::Bishop => "b",
+                    PieceType::Rook => "r",
+                    PieceType::Queen => "q",
+                    PieceType::King => "k",
+                },
+            },
         };
         if i % 8 == 0 {
             println!();
