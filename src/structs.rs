6da36370b618a54ff92358cc9e960948158b9c12
matiasop <movalle4@uc.cl>
2021-09-07 23:58:35 -0300

    delete dead code

--- src/structs.rs
+++ src/structs.rs
@@ -1,26 +1,3 @@
-// #[derive(Clone, Copy, Debug)]
-// pub struct Square {
-//     pub piece: Option<PieceType>,
-//     pub color: Option<bool>, // 0 is white, 1 is black
-// }
-
-// impl Square {
-//     pub fn empty() -> Square {
-//         // returns empty square
-//         Square {
-//             piece: None,
-//             color: None,
-//         }
-//     }
-
-//     pub fn new(piece_type: PieceType, color: bool) -> Square {
-//         Square {
-//             piece: Some(piece_type),
-//             color: Some(color),
-//         }
-//     }
-// }
-
 #[derive(Clone, Copy, Debug)]
 pub struct Piece {
     pub piece_type: PieceType,
